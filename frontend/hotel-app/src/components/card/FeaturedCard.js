6ac066aa50468f006b6ad946deb9102bbb8f9143
NigarRahimli <nigarrahimli07@gmail.com>
2024-07-04 23:27:13 +0400

    properties with facilities  is ready

--- frontend/hotel-app/src/components/card/FeaturedCard.js
+++ frontend/hotel-app/src/components/card/FeaturedCard.js
@@ -1,27 +1,13 @@
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import React from "react";
-
-const steps = [
-  {
-    imgPath: "https://via.placeholder.com/400x255?text=Image+1",
-  },
-  {
-    imgPath: "https://via.placeholder.com/400x255?text=Image+2",
-  },
-  {
-    imgPath: "https://via.placeholder.com/400x255?text=Image+3",
-  },
-  {
-    imgPath: "https://via.placeholder.com/400x255?text=Image+4",
-  },
-];
+import { baseUrl } from "@/components/constant";
 
 const FeaturedCard = ({
   propertyImages,
   isFavourite,
   name,
-  adress,
+  address,
   city,
   country,
   amenities,
@@ -29,7 +15,7 @@ const FeaturedCard = ({
   maxPrice
 }) => {
   const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = steps.length;
+  const maxSteps = propertyImages.length;
 
   React.useEffect(() => {
     const interval = setInterval(() => {
@@ -41,6 +27,7 @@ const FeaturedCard = ({
   const handleStepChange = (step) => {
     setActiveStep(step);
   };
+
   return (
     <div>
       <Box
@@ -66,7 +53,7 @@ const FeaturedCard = ({
           }}
         >
           <img
-            src={steps[activeStep].imgPath}
+            src={propertyImages[activeStep]}
             alt={`step ${activeStep + 1}`}
             style={{ width: "100%", height: "100%" }}
           />
@@ -84,7 +71,7 @@ const FeaturedCard = ({
             mt: 1,
           }}
         >
-          {[...Array(maxSteps)].map((_, index) => (
+          {propertyImages.map((_, index) => (
             <Box
               key={index}
               onClick={() => handleStepChange(index)}
@@ -106,28 +93,25 @@ const FeaturedCard = ({
         </Box>
 
         <img
-          src={
-            isFavourite ? "./icons/full_heart.png" : "./icons/empty_heart.png"
-          }
+          src={isFavourite ? "./icons/full_heart.png" : "./icons/empty_heart.png"}
           className="absolute right-[15px] top-[15px] cursor-pointer"
         />
-        <p className=" font-semibold text-[18px] text-[#9A9A9A] absolute left-[20px] bottom-[15px]">
-          $ {minPrice} - {maxPrice} USD
+        <p className="font-semibold text-[18px] text-[#9A9A9A] absolute left-[20px] bottom-[15px]">
+          ${minPrice} - ${maxPrice} USD
         </p>
       </Box>
 
       <div>
-        <h1 className=" font-bold text-[#484848] text-[18px] mt-[30px]">
+        <h1 className="font-bold text-[#484848] text-[18px] mt-[30px]">
           {name}
         </h1>
-        <p className="font-medium text-[#9A9A9A] text-[14px ] py-[7px]">
-          {" "}
-          {adress}, {city}, {country}{" "}
+        <p className="font-medium text-[#9A9A9A] text-[14px] py-[7px]">
+          {address}, {city}, {country}
         </p>
-        <div className="amenities flex  gap-x-[20px]">
+        <div className="amenities flex gap-x-[20px]">
           {amenities.map((amenity) => (
             <div key={amenity.id} className="flex items-center mr-[4px]">
-              <img src={amenity.iconUrl} alt={amenity.name} className=" mr-1" />
+              <img src={amenity.iconUrl} alt={amenity.name} className="mr-1" />
               <span className="font-semibold text-[#484848] text-[16px]">
                 {amenity.count}
               </span>
--- frontend/hotel-app/src/components/sections/FeaturedProperties.jsx
+++ frontend/hotel-app/src/components/sections/FeaturedProperties.jsx
@@ -1,36 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FeaturedCard from '../card/FeaturedCard';
-
+import { baseUrl } from '@/components/constant';
 
 const FeaturedProperties = () => {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${baseUrl}/api/properties/featured/3`);
+        const data = await response.json();
+        setProperties(data);
+      } catch (error) {
+        console.error('Error fetching properties:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="featured pt-[100px] mx-auto md:w-full w-[279px]">
-      <h1 className="font-bold text-[25px] text-[#484848] md:w-[412px] w-[279px] md:text-[36px] ">
+      <h1 className="font-bold text-[25px] text-[#484848] md:w-[412px] w-[279px] md:text-[36px]">
         Featured Properties on our Listing
       </h1>
       <div className="bg-[#484848] w-[140px] h-[6px] rounded-[3px] mt-[30px] mb-[60px]"></div>
 
       <div className="flex gap-[30px] flex-wrap content-center items-center mx-auto">
-        {[1, 2, 3, 4, 5].map((index) => (
-          <div key={index} className="w-[300px] xl:w-[382px]">
+        {properties.map((property) => (
+          <div key={property.propertyId} className="w-[300px] xl:w-[382px]">
             <FeaturedCard
-              imgSrc="https://images.crowdspring.com/blog/wp-content/uploads/2017/08/23163415/pexels-binyamin-mellish-106399.jpg"
-              userSrc="https://as2.ftcdn.net/v2/jpg/03/83/25/83/1000_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg"
-              isFavourite={false}
-              name="Well Furnished Apartment"
-              location="100 Smart Street, LA, USA"
-              amenities={[
-                {
-                  id: 1,
-                  iconUrl: "./icons/bed.png",
-                  count: 3,
-                },
-                {
-                  id: 2,
-                  iconUrl: "./icons/bath.png",
-                  count: 1,
-                },
-              ]}
+              propertyImages={property.propertyImageDetails.map(image => `${baseUrl}${image.url}`)}
+              isFavourite={property.isLiked}
+              name={property.name}
+              address={property.address}
+              city={property.city}
+              country={property.country}
+              amenities={property.facilitiesDetails.map(facility => ({
+                ...facility,
+                iconUrl: `${baseUrl}${facility.iconUrl}`,
+              }))}
+              minPrice={property.minPrice}
+              maxPrice={property.maxPrice}
             />
           </div>
         ))}
